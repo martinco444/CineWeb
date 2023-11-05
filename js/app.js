88dc4fcad280cc3e721f8c9da415bfe4adca2f2e
MarlonMora23 <146109180+MarlonMora23@users.noreply.github.com>
2023-11-05 13:16:52 -0500

    Update app.js

--- js/app.js
+++ js/app.js
@@ -21,6 +21,7 @@ const originalMovieCards = document.getElementsByClassName("movie__items");
 function searchMovies() {
     const searchInput = document.getElementById("searchInput");
     const searchButton = document.getElementById("searchButton");
+    const clearSearchButton = document.getElementById('clearSearchButton')
 
     searchInput.addEventListener("keydown", function (event) {
         if (event.key === "Enter") {
@@ -40,28 +41,34 @@ function searchMovies() {
         const searchText = searchInput.value.toLowerCase();
         const movieCards = document.getElementsByClassName("movie__items");
 
+        //Colocar todas las películas para la búsqueda
         Array.from(originalMovieCards).forEach(function (originalCard) {
             originalCard.style.display = "flex";
         });
 
-        // Realiza la búsqueda
+        //De todas las películas, buscar las que no coincidan
         Array.from(movieCards).forEach(function (movieCard) {
             const movieTitle = movieCard.querySelector(".movie__title");
+            //Las películas que no coincidan con el titulo, se dejan de ver
             if (movieTitle && !movieTitle.textContent.toLowerCase().includes(searchText)) {
                 movieCard.style.display = "none";
             }
         });
 
+        //actualizar mensaje por si no hay películas
         notMoviesMessage();
     }
 
     function clearSearch() {
+        //borrar el contenido de la barra de búsqueda
         searchInput.value = ''; 
         
+        //mostrar todas las películas 
         Array.from(originalMovieCards).forEach(function (originalCard) {
             originalCard.style.display = "flex";
         });
 
+        //actualizar mensaje por si no hay películas
         notMoviesMessage();
     }
 }
@@ -70,12 +77,12 @@ function searchMovies() {
 function navScroll(){
     const navLinks = document.querySelectorAll('.nav__link');
 
+    //Darle función de desplazamiento a cada link
     navLinks.forEach(link => {
         link.addEventListener('click', function (event) {
             event.preventDefault();
 
             const targetClass = this.getAttribute('href').substring(1);
-
             const targetElement = document.querySelector(`.${targetClass}`);
 
             if (targetElement) {
@@ -97,7 +104,9 @@ function loadMovies() {
     return fetch('/peliculas_data/peliculas.txt')
         .then(response => response.text())
         .then(data => {
+            //Separar cada película mediante un salto de línea
             const movieList = data.split('\n');
+            //Separar cada característica de la película mediante '|'
             const movies = movieList.map(movieData => movieData.split('|'));
             return movies;
         })
@@ -108,10 +117,12 @@ function loadMovies() {
 
 //Cargar funciones
 function loadFunctions() {
-    return fetch('/peliculas_data/funciones.txt') // Devolver la promesa resultante
+    return fetch('/peliculas_data/funciones.txt') 
         .then(response => response.text())
         .then(data => {
+            //Separar cada función de película mediante un salto de línea
             const movieList = data.split('\n');
+             //Separar cada característica de la función de película mediante '|'
             const movies = movieList.map(movieData => movieData.split('|'));
             return movies;
         })
@@ -121,7 +132,7 @@ function loadFunctions() {
 }
 
 function normalizeTitle(title) {
-    // Usa el método normalize para convertir a la forma normalizada (NFC)
+    //método normalize para convertir a la forma normalizada (NFC)
     return title.normalize("NFC").toLowerCase();
 }
 
@@ -130,6 +141,7 @@ function createMovieElements(movieData) {
     const [title, length, classification, genres, cinemaName, originalTitle,
          originCountry, director, actors, lenguage, img, synopsis] = movieData;
 
+    //Crear elementos que muestran las películas
     const movieItem = document.createElement('li');
     movieItem.classList.add('movie__items');
     movieItem.innerHTML = `
@@ -143,7 +155,7 @@ function createMovieElements(movieData) {
 
     // Agregar evento de clic a la imagen
     movieItem.querySelector('.movie__img').addEventListener('click', (event) => {
-        // Encuentra el título de la película a partir de la clase "movie__title"
+        // Encontrar el título de la película
         const titleElement = event.currentTarget.closest('.movie__items').querySelector('.movie__title');
         const title = titleElement.textContent;
 
@@ -156,11 +168,13 @@ function createMovieElements(movieData) {
     return movieItem;
 }
 
+//mostar las peliculas por cine
 function showMoviesForCinema(movies, cinemaName, containerSelector) {
     const cinemaContainer = document.querySelector(containerSelector);
     const movieListElement = cinemaContainer.querySelector('.movie__list');
 
     movies.forEach(movieData => {
+        //Mostar las películas en base al cine
         if (movieData[4] === cinemaName) { 
             const movieItem = createMovieElements(movieData);
             movieListElement.appendChild(movieItem);
@@ -168,6 +182,7 @@ function showMoviesForCinema(movies, cinemaName, containerSelector) {
     });
 }
 
+//Gestion de mensaje en caso de no haber películas disponibles
 function notMoviesMessage() {
     const cinemaSections = document.querySelectorAll(".movies__container");
 
@@ -175,12 +190,12 @@ function notMoviesMessage() {
         const movieList = section.querySelector(".movie__list");
         const movieItems = Array.from(movieList.querySelectorAll(".movie__items"));
         const movieMessage = section.querySelector('.noMovies__message--item');
-        // Verifica si todos los elementos <li> de clase "movie__items" tienen display: none
+
         const allItemsHidden = movieItems.every(function (item) {
             return item.style.display === 'none';
         });
 
-        // Si todos los elementos están ocultos, muestra el mensaje
+        //si movie__list solo tiene el mensaje y no películas o los elementos están escondidos, no hay películas
         if (movieList.children.length === 1 || allItemsHidden) {
             movieList.style.display = 'flex';
             movieList.style.alignItems = 'center';
@@ -189,17 +204,17 @@ function notMoviesMessage() {
             movieMessage.style.display = 'flex';
         }
 
+        //Si hay películas, reestablecer el formato de movieList y esconder el mensaje
         else{
             movieList.style.display = 'grid';
+            movieList.style.justifyContent = 'center';
+            movieList.style.alignItems = 'center';
             movieMessage.style.display = 'none';
         }
     });
 }
 
-
-
-
-
+//Función que inicializa todas las funcionalidades de la página 
 function initialize() {
     loadMovies()
         .then(movies => {
@@ -208,7 +223,8 @@ function initialize() {
             showMoviesForCinema(movies, 'Cinemark', '.movies__cinemark');
             showMoviesForCinema(movies, 'Izimovie', '.movies__izimovie');
             showMoviesForCinema(movies, 'Royalfilms', '.movies__royalfilms');
-            //otras funciones
+
+            //otras funciones de la página
             navResponsive()
             navScroll()
             searchMovies()
