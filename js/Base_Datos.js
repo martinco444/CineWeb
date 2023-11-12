4908c3de5b7b0a3c591381c41eb550c34ec987d4
MarlonMora23 <146109180+MarlonMora23@users.noreply.github.com>
2023-11-12 09:31:46 -0500

    Update Base_Datos.js

--- js/Base_Datos.js
+++ js/Base_Datos.js
@@ -1,30 +1,97 @@
-var bd;
+let bd;
 
-function IniciarBaseDatos()
-{
-    var solicitud = indexedDB.open("Base_CineWeb");
+function openDataBase() {
+    return new Promise((resolve, reject) => {
+        let request = indexedDB.open("Base_CineWeb");
 
-    solicitud.addEventListener("error", MostrarError);
-    solicitud.addEventListener("success", Comenzar);
-    solicitud.addEventListener("upgradeneeded", CrearBase);
+        request.addEventListener("error", (event) => {
+            reject(event);
+        });
+
+        request.addEventListener("success", (event) => {
+            start(event);
+            resolve();  
+        });
+
+        request.addEventListener("upgradeneeded", (event) => {
+            createDataBase(event);
+        });
+    });
 }
 
-function MostrarError(evento){
-    alert("Tenemos un ERROR: " + evento.code + " / " + evento.message);
+function openAndWriteDataBase() {
+    return new Promise((resolve, reject) => {
+        let request = indexedDB.open("Base_CineWeb");
+        let registerButton = document.querySelector('#registerButton');
+
+        registerButton.addEventListener('click', storeClient);
+
+        request.addEventListener("error", (event) => {
+            reject(event);
+        });
+
+        request.addEventListener("success", (event) => {
+            start(event);
+            resolve();
+        });
+
+        request.addEventListener("upgradeneeded", (event) => {
+            createDataBase(event);
+        });
+    });
 }
 
-
-function Comenzar(evento){
-    bd = evento.target.result;
-    
+function start(event) {
+    //Crear variable bd
+    bd = event.target.result;
+    //Obtener la información de la base de datos
+    data()
 }
 
-
-function CrearBase(evento){
-    var basedatos = evento.target.result;
-    var almacen = basedatos.createObjectStore("Email", {KeyPath: "email"});
-    almacen.createIndex("BuscarEmail", "email", {unique: false});
+function createDataBase(evento) {
+    let dataBase = evento.target.result;
+    let store = dataBase.createObjectStore("Email", { keyPath: "email" });
+    store.createIndex("BuscarEmail", "email", { unique: false });
 }
 
+function storeClient() {
+    let emailElement = document.querySelector("#email").value;
+    let passwordElement = document.querySelector("#password").value;
 
-window.addEventListener("load", IniciarBaseDatos);
+    let transaction = bd.transaction(["Email"], "readwrite");
+    let store = transaction.objectStore("Email");
+
+    store.add({ 
+        email: emailElement,
+        password: passwordElement 
+    }
+    , emailElement);
+
+    document.querySelector("#email").value = "";
+    document.querySelector("#password").value = "";
+    localStorage.setItem('nombreBaseDatos', 'Base_CineWeb');
+}
+
+function data(){
+    let transaction = bd.transaction(['Email'])
+    let store = transaction.objectStore('Email')
+    let pointer = store.openCursor()
+
+    pointer.addEventListener('success', writeInfo)
+}
+
+function writeInfo(event){
+    let pointer = event.target.result;
+    if (pointer){
+        data_values[pointer.value.email] = pointer.value.password
+        pointer.continue()
+    }
+}
+
+function getData(){
+    return data_values
+}
+
+let data_values = {}
+
+export {openDataBase, openAndWriteDataBase, getData};
